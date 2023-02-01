376948296fabdff179bf1a2363762bca5a98034d
kaysmart001 <luwoyekayode99@gmail.com>
2023-02-01 19:26:19 +0100

    Update conn1.php

--- conn1.php
+++ conn1.php
@@ -5,13 +5,13 @@ $Password=$_POST['passp'];
 
 //create local variables
 $taken="false";
-$database="grading system";
-$password="";
-$username="root";
+$database="epiz_33459705_testme_db";
+$password="hvZgW4p2guu7";
+$username="epiz_33459705";
 
 
 //connect to database
-      $con = mysql_connect('localhost', $username, $password) or die ("unable to log into database");
+      $con = mysql_connect('sql311.epizy.com', $username, $password) or die ("unable to log into database");
 @mysql_select_db($database, $con) or die ("unable to connect");
 
 mysql_query("INSERT INTO `prayer` VALUES ('$Name', '$Password')") or die ("strange error");
